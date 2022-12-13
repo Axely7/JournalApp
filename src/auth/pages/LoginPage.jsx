303c0ff01c1f158edd267a2dda16bb3b0dc4cf95
Axel Jimenez <axely7@hotmail.com>
2022-12-13 09:33:47 -0600

    init redux and creating thunks for authentication

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,26 +3,49 @@ import { Google } from '@mui/icons-material'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import React from 'react'
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks'
+import { useDispatch } from 'react-redux'
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth'
 
 export const LoginPage = () => {
+
+  const dispatch = useDispatch()
+
+  const {email, password, onInputChange} = useForm({
+    email: 'axely7@hotmail.com',
+    password: '123456'
+  })
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // Disparando thunk
+    dispatch(checkingAuthentication())
+    console.log({email, password})
+  }
+
+  const onGoogleSignIn = () => {
+    dispatch(startGoogleSignIn())
+    console.log('onGoogleSignIn')
+  }
+
   return (
    <AuthLayout title='Login'>
-    <form>
+    <form onSubmit={onSubmit}>
       <Grid container>
         <Grid item xs={12} sx={{mt: 2}}>
-          <TextField label="Correo" type="email" placeholder='correo@google.com' fullWidth/>
+          <TextField label="Correo" type="email" placeholder='correo@google.com' fullWidth name='email' value={email} onChange={onInputChange}/>
         </Grid>
 
         <Grid item xs={12} sx={{mt: 2}}>
-          <TextField label="Contraseña" type="password" placeholder='Contraseña' fullWidth/>
+          <TextField label="Contraseña" type="password" placeholder='Contraseña' fullWidth name='password' value={password} onChange={onInputChange}/>
         </Grid>
 
         <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
           <Grid item xs={12} sm={6}>
-            <Button variant='contained' fullWidth>Login</Button>
+            <Button type="submit" variant='contained' fullWidth>Login</Button>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Button variant='contained' fullWidth><Google /> <Typography sx={{ml:1 }}>Google</Typography> </Button>
+            <Button variant='contained' fullWidth onClick={onGoogleSignIn}><Google /> <Typography sx={{ml:1 }}>Google</Typography> </Button>
           </Grid>
         </Grid>
 
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
-import { MailOutline } from '@mui/icons-material'
-import { Typography } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton, Typography } from '@mui/material'
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
@@ -9,8 +9,21 @@ export const JournalPage = () => {
     <>
       <JournalLayout>
         {/* <Typography>Aute consequat do aliqua excepteur adipisicing sunt minim labore ad aliquip ut consectetur. Sit ipsum sit minim commodo consequat cupidatat aliqua ipsum. Mollit cillum ea nulla in velit exercitation ea nisi exercitation incididunt. Veniam laboris nulla labore ut elit esse dolore ex do. Ex qui enim amet duis qui laborum consequat mollit reprehenderit do in qui id. Officia reprehenderit fugiat fugiat et cupidatat.</Typography> */}
-        {/* <NothingSelectedView /> */}
-        <NoteView />
+        <NothingSelectedView /> 
+        {/* <NoteView /> */}
+        <IconButton 
+          size='large' 
+          sx={{ 
+            color: 'white', 
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', opacity:0.9 },
+            position: 'fixed',
+            right: 50,
+            bottom: 50
+           }}>
+            <AddOutlined sx={{ fontSize: 30 }}/>
+           </IconButton>
+           
       </JournalLayout>
     </>
   
