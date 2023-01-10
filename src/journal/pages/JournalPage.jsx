e7d5bad21f862b32e837a26a45cb09cbc7b0937d
Axel Jimenez <axely7@hotmail.com>
2023-01-10 09:51:15 -0600

    loading notes from firebase

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,14 @@
 import { AddOutlined } from '@mui/icons-material'
 import { IconButton, Typography } from '@mui/material'
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { startNewNote } from '../../store/journal'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
+
+  const { isSaving, active } = useSelector(state => state.journal)
 
   const dispatch = useDispatch();
 
@@ -15,14 +17,15 @@ export const JournalPage = () => {
     dispatch(startNewNote());
   }
 
+  console.log(active)
 
   return (
     <>
       <JournalLayout>
-        {/* <Typography>Aute consequat do aliqua excepteur adipisicing sunt minim labore ad aliquip ut consectetur. Sit ipsum sit minim commodo consequat cupidatat aliqua ipsum. Mollit cillum ea nulla in velit exercitation ea nisi exercitation incididunt. Veniam laboris nulla labore ut elit esse dolore ex do. Ex qui enim amet duis qui laborum consequat mollit reprehenderit do in qui id. Officia reprehenderit fugiat fugiat et cupidatat.</Typography> */}
-        <NothingSelectedView /> 
         {/* <NoteView /> */}
+        {!!active? <NoteView /> :  <NothingSelectedView />}
         <IconButton 
+          disabled={isSaving}
           onClick={onClickNewNote}
           size='large' 
           sx={{ 
