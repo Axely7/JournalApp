66e32c96a4b78ee553c050fefa2b9396b743bbcb
Axel Jimenez <axely7@hotmail.com>
2023-02-09 10:31:17 -0600

    using environment variables for testing

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,10 +2,22 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore/lite'
+import { getEnvironments } from "../helpers";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-console.log(import.meta.env)
+const {
+  VITE_APIKEY,
+  VITE_AUTHDOMAIN,
+  VITE_PROJECTID,
+  VITE_STORAGEBUCKET,
+  VITE_MESSAGINGSENDERID,
+  VITE_APPID,
+  VITE_MEASUREMENTID
+} = getEnvironments();
+
+// console.log(import.meta.env)
+// console.log(process.env)
 
 // Your web app's Firebase configuration
 // Dev/Prod
@@ -19,15 +31,27 @@ console.log(import.meta.env)
 // };
 
 // Testing
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB7DcnY_X72hTcjVhISrhs_wUMDKDSYV88",
+//   authDomain: "react-journal-testing-97e74.firebaseapp.com",
+//   projectId: "react-journal-testing-97e74",
+//   storageBucket: "react-journal-testing-97e74.appspot.com",
+//   messagingSenderId: "536693256387",
+//   appId: "1:536693256387:web:ddcebe41764127709eb276",
+//   measurementId: "G-6Z8HHL4ZER"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB7DcnY_X72hTcjVhISrhs_wUMDKDSYV88",
-  authDomain: "react-journal-testing-97e74.firebaseapp.com",
-  projectId: "react-journal-testing-97e74",
-  storageBucket: "react-journal-testing-97e74.appspot.com",
-  messagingSenderId: "536693256387",
-  appId: "1:536693256387:web:ddcebe41764127709eb276",
-  measurementId: "G-6Z8HHL4ZER"
+  apiKey: VITE_APIKEY,
+  authDomain: VITE_AUTHDOMAIN,
+  projectId: VITE_PROJECTID,
+  storageBucket: VITE_STORAGEBUCKET,
+  messagingSenderId: VITE_MESSAGINGSENDERID,
+  appId: VITE_APPID,
+  measurementId: VITE_MEASUREMENTID
 };
+
+console.log(firebaseConfig)
 
 
 // Initialize Firebase
